8434c398d01ad769f002df28306a24862340b1e8
Felipe <felipevalerioderamos@gmail.com>
2024-06-13 23:11:37 -0300

    small correction

--- main.cc
+++ main.cc
@@ -25,7 +25,6 @@ void generate_song() {
 
 		sample = std::sin(angle * FREQUENCY * static_cast<double>(i));
 		float sample_float = static_cast<float>(sample);
-		// uint8_t buffer[8];
 		uint32_t sample_uint = *reinterpret_cast<uint32_t*>(&sample_float);
 		output_file.write(reinterpret_cast<char*>(&sample_uint), sizeof(sample_uint));
 		// std::cout << "\rWrote: " << (i + 1) * sizeof(sample_uint) << " bytes to " << output_file << std::flush;
@@ -39,21 +38,3 @@ int main() {
 
 	return 0;
 }
-
-
-
-// func generate() {
-// 	nsamps := Duration * SampleRate
-// 	var angle float64 = tau / float64(nsamps)
-// 	file := "out.bin"
-// 	f, _ := os.Create(file)
-// 	for i := 0; i < nsamps; i++ {
-// 		sample := math.Sin(angle * Frequency * float64(i))
-// 		var buf [8]byte
-// 		binary.LittleEndian.PutUint32(buf[:], math.Float32bits(float32(sample)))
-// 		math.Float32bits -> converte float em um inteiro unsigned
-// 		PutUnit32 -> 
-// 		bw,_ := f.Write(buf[:])
-// 		fmt.Printf("\rWrote: %v bytes to %s", bw, file)
-// 	}
-// }
